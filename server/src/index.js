6374a8c050783bf18c5a2a9276b5102a12837695
whatever <whatever>
2025-05-08 17:50:25 +0600

    .

--- server/src/index.js
+++ server/src/index.js
@@ -47,12 +47,12 @@ app.use('/api', routes);
 
 // Serve the React build in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../client/dist')));
-  
+  app.use(express.static(path.join(__dirname, '../../client/build')));
+
   app.get('*', (req, res) => {
     // Don't serve React app for API routes
     if (req.path.startsWith('/api')) return;
-    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
   });
 }
 
